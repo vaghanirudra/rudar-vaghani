428d5e356147436e3a75da4651f79b7983886bcc
vaghanirudra <vaghanirudra75@gmail.com>
2026-03-31 18:48:43 +0530

    Add files via upload

---
+++ rudra.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int mai()
+{
+	int a, b;
+	printf(" enter c and d: ");
+	
+	scanf("%d%d",&c,&d);
+	if (c>d)
+	printf("c munimun later ");
+	printf("d munimun later ");
+	
+	
+}
---
+++ rudra1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int c, d;
+	printf(" enter c and d: ");
+	scanf("%d%d", &c, &d);
+	
+	if (c>d){
+	     printf("c is minimun number : %d", c);
+	}
+	else{    
+	     printf("d is minimun number : %d", d);
+    }
+	
+}
+
+// 
+enter c and d: 34
+12
+c is minimun number : 34
+//
