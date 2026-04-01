b3b1d79e1424d3921054d19e15c8c3e30badd5f9
vaghanirudra <vaghanirudra75@gmail.com>
2026-04-01 18:35:14 +0530

    Add files via upload

---
+++ rudra2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{ 
+   int lenght, base;
+   printf("enter the lenght of rectangle");
+   scanf("%d",&lenght);
+   printf("\nenter the base of rectangle");
+   scanf("%d",&base);
+   printf("area of rectangle= %d",lenght*base);
+}
+
+//
+enter the lenght of rectangle56
+
+enter the base of rectangle54
+area of rectangle= 3024
+//
