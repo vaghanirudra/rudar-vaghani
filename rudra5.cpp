312020ffe1d048762db2eae5e4caa8bcbbb68117
vaghanirudra <vaghanirudra75@gmail.com>
2026-04-01 19:06:19 +0530

    Add files via upload

---
+++ rudra5.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int l;
+	
+	printf("enter the value: ");
+	scanf("%d",&l);
+	
+	printf(" area of square : %d", l*l);
+	
+}
+
+/*
+enter the value: 22
+ area of square : 484
+*/
