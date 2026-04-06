2abfea90722e81fb6a519f1768700dc224fc12c2
vaghanirudra <vaghanirudra75@gmail.com>
2026-04-06 18:34:27 +0530

    Add files via upload

---
+++ loop 1.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+	int n,i;
+	i = 1;
+	printf("enter N:");
+	scanf("%d",&n);
+	while(i<=n){
+		printf("%d\n",i);
+		i = i + 1;
+	}	
+}
+
+/*  
+   
+     enter N:10
+    1
+   	2
+    3
+    4
+   	5
+	6
+	7
+	8
+	9
+	10
