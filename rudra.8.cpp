66cf15122b7d9eb7e67334128350ba24e8dcc38f
vaghanirudra <vaghanirudra75@gmail.com>
2026-04-08 17:53:54 +0530

    Add files via upload

---
+++ rudra.8.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{ 
+    for(char ne='A';ne<='Z';ne++)
+   { 
+        printf("%c ",ne);
+	}
+	
+}
+
+/*
+    output
+    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+*/
